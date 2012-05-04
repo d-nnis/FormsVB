6538d2a8c107d7a18b8149b6e11c7595c9c62e9d
d-nnis <flashgo@gmx.net>
2012-05-04 15:00:09 +0200

    works - except justification

--- TRS_Importfile.vb
+++ TRS_Importfile.vb
@@ -19,18 +19,14 @@ Function OnMngrFormDefTransDefault() As Long
     Dim intRet As Integer, intTRSPos As Integer
     Dim intNoOfFields As Integer, intFieldNo As Integer
     msg = "Do you want a specially designed transaction description?"
-	Style = 4 + 32	' Buttons: Yes&No, QuestionMark
+	Style = 4 + 32	' Buttons: Yes&No, QuestionMark, Default-Button: first
 	intRet = MsgBox(msg, Style, "Transaction Description")
-	
-
-	'MsgBox intRet
  
     If intRet = 7 Then
         'The FORMS default transaction description is used.
         OnMngrFormDefTransDefault = EV_OK ' Return value to FORMS
         Exit Function
     End If
- 
     
     Set objCurrFormDef = ManagerApp.FormDef
     intNoOfFields = objCurrFormDef.FieldDefs.Count
@@ -46,66 +42,39 @@ Function OnMngrFormDefTransDefault() As Long
 	' objFormField.Item(IntFieldNo)
 	' Object: ManagerApp.Formdef.AddTransField(intTRSPos)
 
-	Set objTransField = objCurrFormDef.AddTransField(intTRSPos)
+
 	
+	''' Field
+	Set objTransField = objCurrFormDef.AddTransField(intTRSPos)
 	objTransField.Name = "#Importfile"
 	objTransField.Index = 0
 	objTransField.Format = "X(100)"
+	objTransField.PadChar = " "
+	objTransField.LeftJustify
 	objTransField.Length = 100
-	objTransField.Expression = 100
-	MsgBox objTransField.Name & " hat TRS-Position:" & intTRSPos
-	
+
+
     intRet = objTransField.Save
+	REM MsgBox objTransField.Name & " hat TRS-Position:" & intTRSPos & "intRet:" & intRet
     If intRet <> 0 Then
 		OnMngrFormDefTransDefault = EV_ERROR
 		Exit Function
     End If	
-	
 	intTRSPos = intTRSPos + 1
-	MsgBox "intTRSpos:" & intTRSpos & ", intFieldNo" & intFieldNo
-	
+	''' /Field
+
     'Loops through the fields on the form definition and creates
-    'three transaction fields for each
+    'transaction fields for each
     For intFieldNo = 1 To intNoOfFields
-		
+	' 1 till Count
 		' Com ManagerApp object
 		' ManagerApp.FormDef.FieldDefs.Item(IntFieldNo).Name
-
-		MsgBox "in For-Loop: intTRSpos:" & intTRSpos & ", intFieldNo" & intFieldNo
+		'MsgBox "in For-Loop: intTRSpos:" & intTRSpos & ", intFieldNo" & intFieldNo
+		
 		Set objFormField = objForm.Item(intFieldNo)
-		
-
-		
 			With objFormField 
-			'msg = "nach with: Feldname:" & .Name
-			'MsgBox msg
-				
 				'ManagerApp.FormDef.FieldDefs.Item(intFieldNo)
-				'objFormField.Item(IntFieldNo)
-				
-				'#If
-				' Object:ManagerApp.Formdef.AddTransField(intTRSPos).Name
-				' Object:ManagerApp.Formdef.AddTransField(intTRSPos).Index
-				objTransField.Name = "#If"
-				objTransField.Index = 0
-				objTransField.Format = "N(1)"
-				objTransField.Length = 0
-				If .Index > 0 Then
-					objTransField.Expression = "?" & .Name & _ 
-							"[" & .Index & "]" 'Indexed
-				Else
-					objTransField.Expression = "?" & .Name
-				End If
-	 
-				intRet = objTransField.Save
-				If intRet <> 0 Then
-					OnMngrFormDefTransDefault = EV_ERROR
-					Exit Function
-				End If
-	 
 				'The Field
-				' Object:ManagerApp.FormDef.FieldDefs.Item(IntFieldNo).Name | .Index
-				' 1 till Count
 				intTRSPos = intTRSPos + 1
 				Set objTransField = objCurrFormDef.AddTransField(intTRSPos)
 				' Object:ManagerApp.Formdef.AddTransField(intTRSPos).Name = Object:ManagerApp.FormDef.FieldDefs.Item(IntFieldNo).Name
@@ -114,38 +83,76 @@ Function OnMngrFormDefTransDefault() As Long
 				objTransField.Format = .Format
 				objTransField.Length = .GetMaxLengthFromFormat
 				
-				msg = "Feldname:" & .Name
-				msg = msg & "Feldindex:" & .Index
-				msg = msg & "Feldformat:" & .Format
-				msg = msg & "Feld-maxLength:" & .GetMaxLengthFromFormat
-				msg = msg & "TRS-Position:" & intTRSPos
-				MsgBox msg
+				intRet = objTransField.Save
 				
-	 
-				intRet = objTransField.Save
-				If intRet <> 0 Then
-					OnMngrFormDefTransDefault = EV_ERROR
-					Exit Function
-				End If
-	 
-				intTRSPos = intTRSPos + 1
-				'#EndIf
-				Set objTransField = objCurrFormDef.AddTransField(intTRSPos)
-				objTransField.Name = "#EndIf"
-				objTransField.Index = 0
-				objTransField.Format = "N(1)"
-				objTransField.Length = 0
-	 
-				intRet = objTransField.Save
+				REM msg = "Feldname:" & .Name
+				REM msg = msg & "Feldindex:" & .Index
+				REM msg = msg & "Feldformat:" & .Format
+				REM msg = msg & "Feld-maxLength:" & .GetMaxLengthFromFormat
+				REM msg = msg & "TRS-Position:" & intTRSPos
+				REM msg = msg & "intRet:" & intRet
+				REM MsgBox msg
+
+				
 				If intRet <> 0 Then
 					OnMngrFormDefTransDefault = EV_ERROR
 					Exit Function
 				End If
 	 
 			End With
-			intTRSPos = intTRSPos + 1
+		intTRSPos = intTRSPos + 1
     Next intFieldNo
  
     OnMngrFormDefTransDefault = EV_OK_ABORT  ' Return value to FORMS
  
 End Function
+
+
+	REM ''' Field
+	REM Set objTransField = objCurrFormDef.AddTransField(intTRSPos)
+	REM objTransField.Name = "#Reference"
+	REM objTransField.Index = 0
+	REM objTransField.Format = "-" X(100)"
+	REM objTransField.Length = 100
+    REM intRet = objTransField.Save
+	REM REM MsgBox objTransField.Name & " hat TRS-Position:" & intTRSPos & "intRet:" & intRet
+    REM If intRet <> 0 Then
+		REM OnMngrFormDefTransDefault = EV_ERROR
+		REM Exit Function
+    REM End If	
+	REM intTRSPos = intTRSPos + 1
+	REM ''' /Field
+
+
+
+
+
+
+
+REM alt
+
+	' DocPath1
+	' DocPath2
+	' DstDocPath1
+	' DstDocPath2
+	objTransField.Name = "#Importfile"
+
+REM '#If
+REM ' Object:ManagerApp.Formdef.AddTransField(intTRSPos).Name
+REM ' Object:ManagerApp.Formdef.AddTransField(intTRSPos).Index
+REM objTransField.Name = "#If"
+REM objTransField.Index = 0
+REM objTransField.Format = "N(1)"
+REM objTransField.Length = 0
+REM If .Index > 0 Then
+	REM objTransField.Expression = "?" & .Name & _ 
+			REM "[" & .Index & "]" 'Indexed
+REM Else
+	REM objTransField.Expression = "?" & .Name
+REM End If
+
+REM intRet = objTransField.Save
+REM If intRet <> 0 Then
+	REM OnMngrFormDefTransDefault = EV_ERROR
+	REM Exit Function
+REM End If
