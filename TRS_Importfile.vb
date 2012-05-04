55269ad91e5e3b80b34c5eb32fa1bf8a64c5359e
d-nnis <flashgo@gmx.net>
2012-05-04 13:07:23 +0200

    bevor if gekickt wird

--- TRS_Importfile.vb
+++ TRS_Importfile.vb
@@ -11,102 +11,139 @@ Function OnMngrFormDefTransDefault() As Long
 ' transfer output. All empty fields are skipped.
 ' ---------------------------------------------------------------------
     Dim objTransField As Object
+	Dim objForm As Object
     Dim objFormField As Object
 	Dim objCurrFormDef As Object
     Dim msg As String
-    Dim intRet As Integer, intPos As Integer
+	Dim Style
+    Dim intRet As Integer, intTRSPos As Integer
     Dim intNoOfFields As Integer, intFieldNo As Integer
     msg = "Do you want a specially designed transaction description?"
-    intRet = MsgBox(msg, vbYesNo, "Transaction Description")
+	Style = 4 + 32	' Buttons: Yes&No, QuestionMark
+	intRet = MsgBox(msg, Style, "Transaction Description")
+	
+
+	'MsgBox intRet
  
-    If intRet = vbNo Then
+    If intRet = 7 Then
         'The FORMS default transaction description is used.
         OnMngrFormDefTransDefault = EV_OK ' Return value to FORMS
         Exit Function
     End If
  
-    intPos = 1
+    
     Set objCurrFormDef = ManagerApp.FormDef
     intNoOfFields = objCurrFormDef.FieldDefs.Count
+	MsgBox "intNoFields:" & intNoOfFields & "-"
 	
+	intTRSPos = 1
 	intFieldNo = 1
 
-    Set objFormField = objCurrFormDef.FieldDefs.Item(intFieldNo)
-    Set objTransField = objCurrFormDef.AddTransField(intPos)
+	' Object:ManagerApp.FormDef.FieldDefs.Item(intFieldNo)
+    ' ERR: Set objFormField = objCurrFormDef.FieldDefs.Item(intFieldNo)
 	
+	Set objForm = objCurrFormDef.FieldDefs
+	' objFormField.Item(IntFieldNo)
+	' Object: ManagerApp.Formdef.AddTransField(intTRSPos)
+
+	Set objTransField = objCurrFormDef.AddTransField(intTRSPos)
 	
+	objTransField.Name = "#Importfile"
+	objTransField.Index = 0
+	objTransField.Format = "X(100)"
+	objTransField.Length = 100
+	objTransField.Expression = 100
+	MsgBox objTransField.Name & " hat TRS-Position:" & intTRSPos
 	
-	With objFormField
-		Set objTransField = objCurrFormDef.AddTransField(intPos)
-		objTransField.Name = "#Importfile"
-        objTransField.Index = 0
-        objTransField.Format = "X(100)"
-        objTransField.Length = .GetMaxLengthFromFormat
-		objTransField.Expression = 100
-	End With
     intRet = objTransField.Save
     If intRet <> 0 Then
 		OnMngrFormDefTransDefault = EV_ERROR
 		Exit Function
     End If	
 	
-	intPos = intPos + 1
+	intTRSPos = intTRSPos + 1
+	MsgBox "intTRSpos:" & intTRSpos & ", intFieldNo" & intFieldNo
 	
     'Loops through the fields on the form definition and creates
     'three transaction fields for each
-    For intFieldNo = 2 To intNoOfFields
+    For intFieldNo = 1 To intNoOfFields
+		
+		' Com ManagerApp object
+		' ManagerApp.FormDef.FieldDefs.Item(IntFieldNo).Name
 
- 
-        With objFormField
-            '#If
-            objTransField.Name = "#If"
-            objTransField.Index = 0
-            objTransField.Format = "N(1)"
-            objTransField.Length = 0
-            If .Index > 0 Then
-                                        objTransField.Expression = "?" & .Name & _
-                        "[" & .Index & "]" 'Indexed
-            Else
-                objTransField.Expression = "?" & .Name
-            End If
- 
-            intRet = objTransField.Save
-            If intRet <> 0 Then
-                OnMngrFormDefTransDefault = EV_ERROR
-                Exit Function
-            End If
- 
-            'The Field
-            intPos = intPos + 1
-            Set objTransField = objCurrFormDef.AddTransField(intPos)
-            objTransField.Name = .Name
-            objTransField.Index = .Index
-            objTransField.Format = .Format
-            objTransField.Length = .GetMaxLengthFromFormat
-			MsgBox .Name & " ist objTransField.Name-" & intPos
- 
-            intRet = objTransField.Save
-            If intRet <> 0 Then
-                OnMngrFormDefTransDefault = EV_ERROR
-                Exit Function
-            End If
- 
-            intPos = intPos + 1
-			'#EndIf
-            Set objTransField = objCurrFormDef.AddTransField(intPos)
-            objTransField.Name = "#EndIf"
-            objTransField.Index = 0
-            objTransField.Format = "N(1)"
-            objTransField.Length = 0
- 
-            intRet = objTransField.Save
-            If intRet <> 0 Then
-                OnMngrFormDefTransDefault = EV_ERROR
-                Exit Function
-            End If
- 
-        End With
-        intPos = intPos + 1
+		MsgBox "in For-Loop: intTRSpos:" & intTRSpos & ", intFieldNo" & intFieldNo
+		Set objFormField = objForm.Item(intFieldNo)
+		
+
+		
+			With objFormField 
+			'msg = "nach with: Feldname:" & .Name
+			'MsgBox msg
+				
+				'ManagerApp.FormDef.FieldDefs.Item(intFieldNo)
+				'objFormField.Item(IntFieldNo)
+				
+				'#If
+				' Object:ManagerApp.Formdef.AddTransField(intTRSPos).Name
+				' Object:ManagerApp.Formdef.AddTransField(intTRSPos).Index
+				objTransField.Name = "#If"
+				objTransField.Index = 0
+				objTransField.Format = "N(1)"
+				objTransField.Length = 0
+				If .Index > 0 Then
+					objTransField.Expression = "?" & .Name & _ 
+							"[" & .Index & "]" 'Indexed
+				Else
+					objTransField.Expression = "?" & .Name
+				End If
+	 
+				intRet = objTransField.Save
+				If intRet <> 0 Then
+					OnMngrFormDefTransDefault = EV_ERROR
+					Exit Function
+				End If
+	 
+				'The Field
+				' Object:ManagerApp.FormDef.FieldDefs.Item(IntFieldNo).Name | .Index
+				' 1 till Count
+				intTRSPos = intTRSPos + 1
+				Set objTransField = objCurrFormDef.AddTransField(intTRSPos)
+				' Object:ManagerApp.Formdef.AddTransField(intTRSPos).Name = Object:ManagerApp.FormDef.FieldDefs.Item(IntFieldNo).Name
+				objTransField.Name = .Name
+				objTransField.Index = .Index
+				objTransField.Format = .Format
+				objTransField.Length = .GetMaxLengthFromFormat
+				
+				msg = "Feldname:" & .Name
+				msg = msg & "Feldindex:" & .Index
+				msg = msg & "Feldformat:" & .Format
+				msg = msg & "Feld-maxLength:" & .GetMaxLengthFromFormat
+				msg = msg & "TRS-Position:" & intTRSPos
+				MsgBox msg
+				
+	 
+				intRet = objTransField.Save
+				If intRet <> 0 Then
+					OnMngrFormDefTransDefault = EV_ERROR
+					Exit Function
+				End If
+	 
+				intTRSPos = intTRSPos + 1
+				'#EndIf
+				Set objTransField = objCurrFormDef.AddTransField(intTRSPos)
+				objTransField.Name = "#EndIf"
+				objTransField.Index = 0
+				objTransField.Format = "N(1)"
+				objTransField.Length = 0
+	 
+				intRet = objTransField.Save
+				If intRet <> 0 Then
+					OnMngrFormDefTransDefault = EV_ERROR
+					Exit Function
+				End If
+	 
+			End With
+			intTRSPos = intTRSPos + 1
     Next intFieldNo
  
     OnMngrFormDefTransDefault = EV_OK_ABORT  ' Return value to FORMS
