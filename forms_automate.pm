4916e8f18de588dc36a7ea6df40150e0b01fdd10
d-nnis <flashgo@gmx.net>
2012-05-23 16:40:31 +0200

    works: std1

--- forms_automate.pm
+++ forms_automate.pm
@@ -12,8 +12,7 @@ use Win32::GuiTest qw (:ALL);
 		my $self = {};
 		bless ($self, $class);
 		# default settings
-		$self->option(save_next => 1);
-		$self->option(shell => 1);
+		$self->option(save_next => 1, shell => 1, wait => 50);
 		return $self;
 	}
 	
@@ -47,7 +46,7 @@ use Win32::GuiTest qw (:ALL);
             }
         }
         my (%opts_in) = @_;
-        my @valid_opts = qw(save_next shell);
+        my @valid_opts = qw(save_next shell wait);
         my $option;
         foreach my $opt_in (keys %opts_in) {
             warn "not recognized option: $opt_in" unless grep {$opt_in eq $_} @valid_opts;
@@ -56,22 +55,36 @@ use Win32::GuiTest qw (:ALL);
         }
     }
 	
-	sub TRS_std_1 {
+	sub std1 {
 		my $self = shift;
 		$self->init() if $self->option("shell");
 		$self->option(save_next => 0);
 		$self->TRS_std();
 		$self->change_format_TRS();
+		$self->form_def_einst();
 		$self->save();
+	}
+	
+	## ci: continue item
+	sub ci {
+		my $self = shift;
+		Win32::GuiTest::SendKeys("%bf");
+	}
+	
+	sub form_def_einst {
+		my $self = shift;
+		Win32::GuiTest::SendKeys("%bf");
+		Win32::GuiTest::SendKeys("k{ENTER}",$self->get_option("wait"));
 	}
 	
 	# set standard TRS, overwrite existing TRS
 	sub TRS_std {
 		my $self = shift;
+		my $wait = $self->get_option("wait");
 		# open TRS
 		Win32::GuiTest::SendKeys("%bt");
 		# default TRS on empty
-		Win32::GuiTest::SendKeys("{TAB}{ENTER}{DOWN}{ENTER}{ENTER}{ENTER}",50);
+		Win32::GuiTest::SendKeys("{TAB}{ENTER}{DOWN}{ENTER}{ENTER}{ENTER}",$wait);
 		print "std in TRS\n";
 		# close or empty
 		$self->save_next() if $self->get_option("save_next");
@@ -80,44 +93,46 @@ use Win32::GuiTest qw (:ALL);
 	# changes Importfile & Fieldfile tp  "X(100)", requires standard TRS
 	sub change_format_TRS {
 		my $self = shift;
+		my $length = shift || 100;
+		my $wait = $self->get_option("wait");
 		# open TRS
 		Win32::GuiTest::SendKeys("%bt");
 		# select #Importfile
 		foreach (1..11) {
-			Win32::GuiTest::SendKeys("{DOWN}",35);	
+			Win32::GuiTest::SendKeys("{DOWN}",$wait);	
 		}
 		# find properties button
 		foreach (1..15) {
-			Win32::GuiTest::SendKeys("{TAB}",35);	
+			Win32::GuiTest::SendKeys("{TAB}",$wait);	
 		}
 		# Format field
-		Win32::GuiTest::SendKeys("{ENTER}{TAB}{TAB}",35);
-		# enter "X(100)"
-		Win32::GuiTest::SendKeys("{X}{(}100{)}");
+		Win32::GuiTest::SendKeys("{ENTER}{TAB}{TAB}",$wait);
+		# enter "X($length)"
+		#Win32::GuiTest::SendKeys("{X}{(}100{)}");
+		Win32::GuiTest::SendKeys("{X}{(}($length){)}");
 		# length field
-		Win32::GuiTest::SendKeys("{TAB}{TAB}{TAB}{TAB}",35);
-		# enter "X(100)"
-		Win32::GuiTest::SendKeys("100");
-		Win32::GuiTest::SendKeys("{ENTER}",50);
+		Win32::GuiTest::SendKeys("{TAB}{TAB}{TAB}{TAB}",$wait);
+		Win32::GuiTest::SendKeys($length);
+		Win32::GuiTest::SendKeys("{ENTER}",$wait);
 		print "importfile changed\n";
 		
 		# select Fieldfile
 		Win32::GuiTest::SendKeys("{DOWN}{DOWN}");
 		# find properties button
 		foreach (1..15) {
-			Win32::GuiTest::SendKeys("{TAB}",35);	
+			Win32::GuiTest::SendKeys("{TAB}",$wait);	
 		}
 		# Format field
-		Win32::GuiTest::SendKeys("{ENTER}{TAB}{TAB}",35);
+		Win32::GuiTest::SendKeys("{ENTER}{TAB}{TAB}",$wait);
 		# enter "X(100)"
-		Win32::GuiTest::SendKeys("{X}{(}100{)}");
+		Win32::GuiTest::SendKeys("{X}{(}($length){)}");
 		# length field
-		Win32::GuiTest::SendKeys("{TAB}{TAB}{TAB}{TAB}",35);
-		# enter "X(100)"
-		Win32::GuiTest::SendKeys("100");
-		Win32::GuiTest::SendKeys("{ENTER}",35);
+		Win32::GuiTest::SendKeys("{TAB}{TAB}{TAB}{TAB}",$wait);
+		# enter "100"
+		Win32::GuiTest::SendKeys($length);
+		Win32::GuiTest::SendKeys("{ENTER}",$wait);
 		print "fieldfile changed\n";
-		Win32::GuiTest::SendKeys("{ENTER}",35);
+		Win32::GuiTest::SendKeys("{ENTER}",$wait);
 		$self->save_next() if $self->get_option("save_next");
 	}
 	
@@ -130,14 +145,14 @@ use Win32::GuiTest qw (:ALL);
 	sub save {
 		my $self = shift;
 		# save
-		Win32::GuiTest::SendKeys("^s",35);
+		Win32::GuiTest::SendKeys("^s",$self->get_option("wait"));
 		print "def saved";
 	}
 	
 	sub next {
 		my $self = shift;
 		# to next and load
-		Win32::GuiTest::SendKeys("{TAB}{DOWN}{ENTER}",35);
+		Win32::GuiTest::SendKeys("{TAB}{DOWN}{ENTER}",$self->get_option("wait"));
 	}
 }
 
